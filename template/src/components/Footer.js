ed16696f9314a5387148a6864e5babb99e28a95d
Chiara Marcial Martínez <contact@cofuente.io>
2023-01-18 11:18:54 -0500

    remove superfluous code
    Signed-off-by: Chiara Marcial Martínez <contact@cofuente.io>

--- template/src/components/Footer.js
+++ template/src/components/Footer.js
@@ -1,6 +1,3 @@
-
-import React from 'react'
-
 export const Footer = ( { } ) => (
   <footer className="love">
     <p>Made with <span role="img" aria-label="red heart">❤️</span> by <a className="App-link" href="https://cofuente.io" target="_blank" rel="noopener noreferrer">cofuente</a>.</p>
